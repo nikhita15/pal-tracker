d387eea0117740b29f7d90695806a5b838fcf670
Nikhita Jalan <nikhita.jalan@gmail.com>
2018-01-30 16:54:25 -0500

    changes for councourse pipeline

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,5 +1,6 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeController {
 
+    String welcomeMessage;
+
+    public WelcomeController(@Value("${WELCOME_MESSAGE}") String message) {
+        this.welcomeMessage=message;
+    }
+
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return welcomeMessage;
     }
 }
