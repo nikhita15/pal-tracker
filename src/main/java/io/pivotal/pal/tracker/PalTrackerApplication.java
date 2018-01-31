c363d3ab3f7d4ed8a4290a35626a7d0f915fd2fb
Nikhita Jalan <nikhita.jalan@gmail.com>
2018-01-31 13:44:13 -0500

    test file

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -7,8 +7,10 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
+@ComponentScan
 @SpringBootApplication
 public class PalTrackerApplication {
 
